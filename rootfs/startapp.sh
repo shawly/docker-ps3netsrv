63a117b168c5ad6e53a0e1f0b37047d602ecc6e3
shawly <shawlyde@gmail.com>
2018-03-04 02:57:41 +0100

    Changed logging.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -2,4 +2,4 @@
 # NOTE: Change the workdir to /games, as this is the default location when
 #       opening files.
 cd /games
-exec /opt/ps3netsrv/ps3netsrv /games
+exec /opt/ps3netsrv/ps3netsrv /games >> /var/log/ps3netsrv.log 2>&1
