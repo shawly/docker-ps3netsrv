4b8b2d3706103d83123e583779529aadb008c6d5
shawly <shawlyde@gmail.com>
2018-03-04 02:30:44 +0100

    Fixed startup file.

--- rootfs/etc/cont-init.d/ps3netsrv.sh
+++ rootfs/etc/cont-init.d/ps3netsrv.sh
@@ -4,6 +4,6 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 # Take ownership of the games directory content.
-chown -R $USER_ID:$GROUP_ID /games/*
+chown -R $USER_ID:$GROUP_ID /games
 
 # vim: set ft=sh :
