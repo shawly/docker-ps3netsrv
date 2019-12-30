e36d53d507255f2f65913815df0edb57ecbdd6ea
shawly <shawly@users.noreply.github.com>
2019-12-30 23:09:07 +0059

    Changed builder to same image as app, since libraries were inconistent.

--- ps3netsrv-builder/run.sh
+++ ps3netsrv-builder/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SCRIPT_DIR="$(readlink -f "$(dirname "$0")")"
-BUILDER_DOCKER_IMAGE="ubuntu:xenial"
+BUILDER_DOCKER_IMAGE=jlesage/baseimage:alpine-3.9-glibc
 
 exec docker run --rm \
     -v "$SCRIPT_DIR:/output" \
     -v "$SCRIPT_DIR/builder:/builder:ro" \
-    "$BUILDER_DOCKER_IMAGE" /builder/build.sh /output
+    $BUILDER_DOCKER_IMAGE /builder/build.sh /output
