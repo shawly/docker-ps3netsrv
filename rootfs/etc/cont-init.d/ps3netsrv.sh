29278dc67aa57c01052885b40bf20f6bf83f866e
shawly <shawlyde@gmail.com>
2024-02-15 14:06:07 +0100

    Changed logging.

--- rootfs/etc/cont-init.d/ps3netsrv.sh
+++ rootfs/etc/cont-init.d/ps3netsrv.sh
@@ -4,6 +4,6 @@ set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
 # Take ownership of the games directory content.
-chown -R $USER_ID:$GROUP_ID /games
+chown -R $USER_ID:$GROUP_ID /games >> /var/log/ps3netsrv.log 2>&1
 
 # vim: set ft=sh :
