1d7a306f2f2df148c40d7aa68aac39408190de7d
shawly <shawlyde@gmail.com>
2024-02-15 14:06:07 +0100

    Fixed logging.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -2,5 +2,4 @@
 # NOTE: Change the workdir to /games, as this is the default location when
 #       opening files.
 cd /games
-touch /var/log/ps3netsrv.log
 exec /opt/ps3netsrv/ps3netsrv /games >> /var/log/ps3netsrv.log 2>&1
