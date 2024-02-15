b1934068fb496fad49ba735f59a278c0fb5c999a
shawly <shawlyde@gmail.com>
2024-02-15 14:06:07 +0100

    Fixed naming issues.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -2,4 +2,4 @@
 # NOTE: Change the workdir to /games, as this is the default location when
 #       opening files.
 cd /games
-exec env HOME=/games /opt/ps3netsvr/ps3netsvr $HOME
+exec env HOME=/games /opt/ps3netsrv/ps3netsrv $HOME
